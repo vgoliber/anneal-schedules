2b844a3950f8dff4caf7a99be1c0baff1521f8cc
Victoria Goliber <38987205+vgoliber@users.noreply.github.com>
2022-02-03 09:43:22 +0100

    Use precomputed embedding

--- anneal_schedules_2.py
+++ anneal_schedules_2.py
@@ -1,9 +1,10 @@
 # ------ Import necessary packages ----
 
 from collections import defaultdict
+import json
 
 from dwave.system.samplers import DWaveSampler
-from dwave.system.composites import EmbeddingComposite
+from dwave.system.composites import FixedEmbeddingComposite
 from dimod import BQM
 
 # choose exactly two BQM
@@ -12,13 +13,18 @@ n = 100
 bqm = BQM('BINARY')
 
 for i in range(n):
-    bqm.add_linear(i, -3)
+    bqm.add_linear(str(i), -3)
     for j in range(i+1,n):
-        bqm.add_quadratic(i, j, 2)
+        bqm.add_quadratic(str(i), str(j), 2)
 
 bqm.offset = 4
 
-sampler = EmbeddingComposite(DWaveSampler(solver='Advantage_system4.1'))
+# Retrieve precomputed embedding
+embedding = {}
+with open('embedding.json') as json_file:
+     embedding = json.load(json_file)
+
+sampler = FixedEmbeddingComposite(DWaveSampler(solver='Advantage_system4.1'), embedding=embedding)
 sampleset = sampler.sample(bqm, num_reads=1000)
 
 print(sampleset)

0f67238bde8aafe0225b7e456958e8b7ed1e9061
Victoria Goliber <38987205+vgoliber@users.noreply.github.com>
2022-02-03 09:28:29 +0100

    Create anneal_schedules_3.py

---
+++ anneal_schedules_3.py
@@ -0,0 +1,27 @@
+# ------ Import necessary packages ----
+
+from collections import defaultdict
+
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+from dimod import BQM
+
+# choose exactly two BQM
+n = 100
+
+bqm = BQM('BINARY')
+
+for i in range(n):
+    bqm.add_linear(i, -3)
+    for j in range(i+1,n):
+        bqm.add_quadratic(i, j, 2)
+
+bqm.offset = 4
+schedule = [ [0.0, 0.0], [20.0, 1.0] ]
+
+sampler = EmbeddingComposite(DWaveSampler(solver='Advantage_system4.1'))
+sampleset = sampler.sample(bqm, num_reads=1000, anneal_schedule=schedule)
+
+print(sampleset)
+
+print("\nNumber chosen:", sum(sampleset.first.sample.values()))
